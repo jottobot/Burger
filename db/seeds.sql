228f86e579317f64aabf4cc5fe5b29c13548af34
Jordynn Otto <j_taylor92@yahoo.com>
2019-05-31 13:02:28 -0700

    set up files and started app

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('Chili Burger');
+INSERT INTO burgers (name) VALUES ('Green Goddess (Vegan) Burger');
+INSERT INTO burgers (name) VALUES ('1/4 Pound with Egg and Cheese Burger');
