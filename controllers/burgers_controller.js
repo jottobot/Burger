10918e396547aa416075c703e9d3519a72490d1e
Jordynn Otto <j_taylor92@yahoo.com>
2019-06-03 12:42:16 -0700

    almost done

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,8 +1,7 @@
 var express = require("express");
-
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
+// Import the model to use its database functions.
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
@@ -17,24 +16,23 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.create([
-    "burger_name", "devoured"
-  ], [
-    req.body.burger_name, req.body.devoured
-  ], function(result) {
+  burger.create(
+    ["burger_name", "devoured"], 
+  [req.body.name, req.body.devoured], 
+  function(result) {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
 router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
+  var condition = `id = ${req.params.id}`;
   console.log("condition", condition);
-
   burger.update({
     devoured: req.body.devoured
-  }, condition, function(result) {
+  }, 
+  condition, 
+  function(result) {
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
@@ -44,18 +42,18 @@ router.put("/api/burgers/:id", function(req, res) {
   });
 });
 
-router.delete("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+// router.delete("/api/burgers/:id", function(req, res) {
+//   var condition = `id = ${req.params.id}`;
 
-  burger.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
+//   burger.delete(condition, function(result) {
+//     if (result.affectedRows == 0) {
+//       // If no rows were changed, then the ID must not exist, so 404
+//       return res.status(404).end();
+//     } else {
+//       res.status(200).end();
+//     }
+//   });
+// });
 
 // Export routes for server.js to use.
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 var express = require("express");
-
-var PORT = process.env.PORT || 8080;
+var bodyParser = require('body-parser');
 
 var app = express();
-
+var PORT = process.env.PORT || 8080;
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
 // Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
