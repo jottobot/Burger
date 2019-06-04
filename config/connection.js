9b4bdbb2cd46f3b6b529cfb01138419786d17353
Jordynn Otto <j_taylor92@yahoo.com>
2019-06-04 16:05:49 -0700

    schema change for heroku

--- config/connection.js
+++ config/connection.js
@@ -18,5 +18,17 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL); 
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "LMUgrad2014",
+    database: "burgers_db"
+  });  
+}
+
 // Export connection for our ORM to use.
 module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,10 @@ USE burgers_db;
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
+    description VARCHAR(255),
+    createdAt TIMESTAMP NOT NULL DEFAULT current_timestamp ON UPDATE CURRENT_TIMESTAMP,
 	burger_name varchar(255) NOT NULL,
-	devoured BOOLEAN DEFAULT false,
+	devoured BOOLEAN DEFAULT true,
+    
 	PRIMARY KEY (id)
 );
